a5f40cb3c203fff3e51ec11eafa358a6c65df9fe
Tayler Porter <taylerporter@gmail.com>
2022-07-15 22:51:31 -0500

    Added rounding to trust

--- etn/helpers.py
+++ etn/helpers.py
@@ -98,7 +98,7 @@ def get_votes(_for: int, _from: int) -> float:
     print("Total Votes:")
     print(total_votes - for_user_votes)
     print("Score: ")
-    score = scores[for_index] * (total_votes - for_user_votes)
+    score = round(scores[for_index] * (total_votes - for_user_votes), 2)
     if score == -0.0:
         score = 0.0
     print(score)
