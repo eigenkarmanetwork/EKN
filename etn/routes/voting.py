498cb49ad8f81b703b17793e9974cf0f1138406d
Tayler Porter <taylerporter@gmail.com>
2022-07-19 20:50:38 -0500

    Migrated vote route to new HTTP Status Code convention.   404: Service was not found. -> 403: Service name or key is incorrect.   403: Username or Password is incorrect. -> 400: User cannot vote for themselves.   403: Username or Password is incorrect. -> 404: 'to' is not connected to this service. Replaced service name/key verification, from username/password verification, and to username resolving code with helper functions.

--- etn/routes/voting.py
+++ etn/routes/voting.py
@@ -1,6 +1,6 @@
 from etn.database import DatabaseManager
 from etn.decs import allow_cors
-from etn.helpers import get_params, get_votes
+from etn.helpers import get_params, get_votes, resolve_service_username, verify_credentials, verify_service
 from flask import Response, request
 import hashlib
 import json
@@ -19,62 +19,46 @@ def vote() -> Response:
     }
 
     Returns:
-    404: Service was not found.  # This will be returned if the name or key is incorrect.
-    403: Username or Password is incorrect.  # For security reasons this will also be returned if to is not found or if they vote for themselves.
+    400: User cannot vote for themselves.
+    403: Username or Password is incorrect.
+    403: Service name or key is incorrect.
+    404: 'to' is not connected to this service.
     200: Success.
     """
     service, key, to, _from, password = get_params(["service_name", "service_key", "to", "from", "password"])
 
-    with DatabaseManager() as db:
-        result = db.execute("SELECT * FROM services WHERE name=:name", {"name": service})
-        service_obj = result.fetchone()
-        if not service_obj:
-            return Response("Service was not found.", 404)
-        salt = service_obj["salt"]
-        sha512 = hashlib.new("sha512")
-        sha512.update(f"{key}:{salt}".encode("utf8"))
-        key_hash = sha512.hexdigest()
-        if key_hash != service_obj["key"]:
-            return Response("Service was not found.", 404)
-        result = db.execute("SELECT * FROM connections WHERE service=:service_id AND service_user=:from",
-                            {"service_id": service_obj["id"], "from": _from})
-        from_service_user = result.fetchone()
-        if not from_service_user:
-            return Response("Username or Password is incorrect.", 403)
-        result = db.execute("SELECT * FROM users WHERE id=:id", {"id": from_service_user["user"]})
-        user = result.fetchone()
-        if not user:
-            return Response("Username or Password is incorrect.", 403)
-    salt = user["salt"]
-    sha512 = hashlib.new("sha512")
-    sha512.update(f"{password}:{salt}".encode("utf8"))
-    password_hash = sha512.hexdigest()
-    if password_hash != user["password"]:
+    if _from == to:
+        return Response("User cannot vote for themselves.", 400)
+
+    service_obj = verify_service(service, key)
+    if not service_obj:
+        return Response("Service name or key is incorrect.", 403)
+    user = resolve_service_username(service_obj["id"], _from)
+    if not user:
         return Response("Username or Password is incorrect.", 403)
+    from_user = verify_credentials(user["username"], password)
+    if not from_user:
+        return Response("Username or Password is incorrect.", 403)
+    to_user = resolve_service_username(service_obj["id"], to)
+    if not to_user:
+        return Response("'to' is not connected to this service.", 404)
+
     with DatabaseManager() as db:
-        result = db.execute("SELECT * FROM connections WHERE service=:service_id AND service_user=:to",
-                            {"service_id": service_obj["id"], "to": to})
-        to_service_user = result.fetchone()
-        if not to_service_user:
-            return Response("Username or Password is incorrect.", 403)
-        result = db.execute("SELECT * FROM users WHERE id=:id", {"id": to_service_user["user"]})
-        to_user = result.fetchone()
-        if not to_user:
-            return Response("Username or Password is incorrect.", 403)
-        from_user = user
-
-        if from_user["id"] == to_user["id"]:
-            return Response("Username or Password is incorrect.", 403)
-
-        result = db.execute("SELECT * FROM votes WHERE user_from=:from AND user_to=:to",
-                            {"from": from_user["id"], "to": to_user["id"]})
+        result = db.execute(
+            "SELECT * FROM votes WHERE user_from=:from AND user_to=:to",
+            {"from": from_user["id"], "to": to_user["id"]},
+        )
         current = result.fetchone()
         if current:
-            db.execute("UPDATE votes SET count=:count WHERE user_from=:from AND user_to=:to",
-                       {"from": from_user["id"], "to": to_user["id"], "count": current["count"] + 1})
+            db.execute(
+                "UPDATE votes SET count=:count WHERE user_from=:from AND user_to=:to",
+                {"from": from_user["id"], "to": to_user["id"], "count": current["count"] + 1},
+            )
         else:
-            db.execute("INSERT INTO votes (user_from, user_to, count) VALUES (?, ?, 1)",
-                       (from_user["id"], to_user["id"]))
+            db.execute(
+                "INSERT INTO votes (user_from, user_to, count) VALUES (?, ?, 1)",
+                (from_user["id"], to_user["id"]),
+            )
 
     return Response("Success", 200)
 
@@ -101,9 +85,11 @@ def get_score() -> Response:
         "score": float
     }
     """
-    service, key, _for, _from, password = get_params(["service_name", "service_key", "for", "from", "password"])
+    service, key, _for, _from, password = get_params(
+        ["service_name", "service_key", "for", "from", "password"]
+    )
     if _for == _from:
-            return Response("Username or Password is incorrect.", 403)
+        return Response("Username or Password is incorrect.", 403)
     with DatabaseManager() as db:
         result = db.execute("SELECT * FROM services WHERE name=:name", {"name": service})
         service_obj = result.fetchone()
@@ -115,8 +101,10 @@ def get_score() -> Response:
         key_hash = sha512.hexdigest()
         if key_hash != service_obj["key"]:
             return Response("Service was not found.", 404)
-        result = db.execute("SELECT * FROM connections WHERE service=:service_id AND service_user=:from",
-                            {"service_id": service_obj["id"], "from": _from})
+        result = db.execute(
+            "SELECT * FROM connections WHERE service=:service_id AND service_user=:from",
+            {"service_id": service_obj["id"], "from": _from},
+        )
         from_service_user = result.fetchone()
         if not from_service_user:
             print("From user doesn't exist for service")
@@ -134,8 +122,10 @@ def get_score() -> Response:
         print("Password is incorrect")
         return Response("Username or Password is incorrect.", 403)
     with DatabaseManager() as db:
-        result = db.execute("SELECT * FROM connections WHERE service=:service_id and service_user=:for",
-                            {"service_id": service_obj["id"], "for": _for})
+        result = db.execute(
+            "SELECT * FROM connections WHERE service=:service_id and service_user=:for",
+            {"service_id": service_obj["id"], "for": _for},
+        )
         for_service_user = result.fetchone()
         if not for_service_user:
             print("For user doesn't exist for service")
