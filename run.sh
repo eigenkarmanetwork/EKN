e732eefd1df1019912935f912b94db5fe63f7277
Tayler Porter <taylerporter@gmail.com>
2022-08-02 17:14:24 -0500

    Adding more logging to the gunicorn server

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-gunicorn --bind 0.0.0.0:31415 app:app
+gunicorn --bind 0.0.0.0:31415 --log-level debug --access-logfile /var/log/etn/etn_access.log --error-logfile /var/log/etn/etn_error.log app:app
