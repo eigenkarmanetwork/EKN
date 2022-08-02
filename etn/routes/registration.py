b8dfd521d715d620717ad5913b874e3beace3ed2
Tayler Porter <taylerporter@gmail.com>
2022-08-02 11:27:28 -0500

    Changed register_connection route to provide the same response as verify_credentials.

--- etn/routes/registration.py
+++ etn/routes/registration.py
@@ -86,7 +86,12 @@ def register_connection() -> Response:
     Returns:
     403: Service name or key is incorrect.
     403: Username or Password is incorrect.
-    200: Connection key if security is 0, otherwise nothing.
+    200: JSON:
+    {
+        "password": str
+        "password_type": Literal["password_hash", "conneciton_key", "session_key"]
+        "expires": int (unix timestamp or 0 if N/A)
+    }
     """
     service, key, service_user, username, password, password_type = get_params(
         ["service_name", "service_key", "service_user", "username", "password", "password_type"]
@@ -95,34 +100,76 @@ def register_connection() -> Response:
     service_obj = verify_service(service, key)
     if not service_obj:
         return Response("Service name or key is incorrect.", 403)
+    service_id = service_obj["id"]
 
-    user = verify_credentials(username, password, password_type, service_obj["id"])
+    user = verify_credentials(username, password, password_type, service_id)
     if not user:
         return Response("Username or Password is incorrect.", 403)
 
     with DatabaseManager() as db:
         result = db.execute(
-            "SELECT * FROM connections WHERE service=:service_id AND user=:user_id",
-            {"service_id": service_obj["id"], "user_id": user["id"]},
+            "SELECT * FROM connections WHERE user=:user_id AND service=:service_id",
+            {"user_id": user["id"], "service_id": service_id},
         )
-        if user["security"] == 0:
-            connection_key = secrets.token_hex(16)
-        else:
-            connection_key = None
-        if result.fetchone():
-            # Connection with this service already exists, updating.
+        row = result.fetchone()
+        if not row:
             db.execute(
-                "UPDATE connections SET service_user=:service_user, key=:key WHERE service=:service_id AND user=:user_id",
-                {
-                    "service_user": service_user,
-                    "key": connection_key,
-                    "service_id": service_obj["id"],
-                    "user_id": user["id"],
-                },
+                "INSERT INTO connections (service, service_user, user) VALUES (?, ?, ?)",
+                (service_id, service_user, user["id"])
             )
-        else:
-            db.execute(
-                "INSERT INTO connections (service, service_user, user, key) VALUES (?, ?, ?, ?)",
-                (service_obj["id"], service_user, user["id"], connection_key),
+    session_key: Optional[str] = None
+    connection_key: Optional[str] = None
+    expires = 0
+    if user["security"] == 0:
+        with DatabaseManager() as db:
+            result = db.execute(
+                "SELECT * FROM connections WHERE user=:user_id AND service=:service_id",
+                {"user_id": user["id"], "service_id": service_id},
             )
-    return Response(connection_key if connection_key else "", 200)
+            row = result.fetchone()
+            if not row:
+                return Response("Service is not connected.", 500)
+            if row["key"]:
+                connection_key = row["key"]
+            else:
+                # Security was set to 0 after the connection was made, generating key.
+                connection_key = secrets.token_hex(16)
+                db.execute(
+                    "UPDATE connections SET key=:connection_key WHERE user=:user_id AND service=:service_id",
+                    {"connection_key": connection_key, "user_id": user["id"], "service_id": service_id},
+                )
+    if user["security"] == 1:
+        with DatabaseManager() as db:
+            result = db.execute("SELECT * FROM session_keys WHERE user=:user_id", {"user_id": user["id"]})
+            row = result.fetchone()
+            gen_key = True
+            if row:
+                gen_key = False
+                if row["expires"] > int(time.time()):
+                    session_key = row["key"]
+                    expires = row["expires"]
+                else:
+                    session_key = secrets.token_hex(16)
+                    expires = int(time.time()) + 86_400
+                    db.execute(
+                        "UPDATE session_keys SET key=:key, expires=:expires WHERE user=:id",
+                        {"id": user["id"], "key": session_key, "expires": expires},
+                    )
+            if gen_key:
+                session_key = secrets.token_hex(16)
+                expires = int(time.time()) + 86_400
+                db.execute(
+                    "INSERT INTO session_keys (user, key, expires) VALUES (?, ?, ?)",
+                    (user["id"], session_key, expires),
+                )
+
+    resp = {
+        "password": connection_key if connection_key else session_key if session_key else user["password"],
+        "password_type": "connection_key"
+        if connection_key
+        else "session_key"
+        if session_key
+        else "password_hash",
+        "expires": expires if expires else 0,
+    }
+    return Response(json.dumps(resp), 200)
