e71d8880d2aea22d15762167d2e0e26a12c37aac
Tayler Porter <taylerporter@gmail.com>
2022-08-11 19:16:11 -0500

    Ran black over all files.

--- scripts/vote.py
+++ scripts/vote.py
@@ -8,6 +8,12 @@ service_key = input("Service Key: ")
 to_user = input("To: ")
 from_user = input("From: ")
 password = getpass.getpass("Password: ")
-data = {"service_name": service_name, "service_key": service_key, "to": to_user, "from": from_user, "password": password}
+data = {
+    "service_name": service_name,
+    "service_key": service_key,
+    "to": to_user,
+    "from": from_user,
+    "password": password,
+}
 r = requests.post("http://127.0.0.1:31415/vote", data=json.dumps(data), headers=headers)
 print(f"{r.status_code}: {r.text}")
