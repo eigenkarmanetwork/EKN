757cf3ddddb9ec5099fabaede977f0d968dc7fa0
Tayler Porter <taylerporter@gmail.com>
2022-08-14 14:02:39 -0500

    Changed start scripts from using FLASK_ENV to FLASK_DEBUG environ var due to FLASK_ENV deprecation.

--- start.bat
+++ start.bat
@@ -1,5 +1,5 @@
 @echo off
 set FLASK_RUN_HOST=0.0.0.0
 set FLASK_RUN_PORT=31415
-set FLASK_ENV=development
+set FLASK_DEBUG=True
 python -m flask run
--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 export FLASK_RUN_HOST=0.0.0.0
 export FLASK_RUN_PORT=31415
-export FLASK_ENV=development
+export FLASK_DEBUG=True
 python -m flask run
